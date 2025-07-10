0c5568ae24ea9505bc3fc3573732b708661a2217
mwthot <mw.thot@gmail.com>
2025-07-10 14:36:32 -0700

    finished player section

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0 
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True: 
@@ -21,9 +26,10 @@ def main():
             if event.type == pygame.QUIT: 
                 return
         
-        screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        screen.fill("black")
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
