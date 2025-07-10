9dede218d8111b93a9d093e32d6bfffaedfc1648
mwthot <mw.thot@gmail.com>
2025-07-10 14:21:38 -0700

    added rotation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from player import * 
+from circleshape import * 
 from constants import *
 
 def main():
@@ -6,14 +8,27 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0 
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True: 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                 return
+        
         screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+    
 
 
 if __name__ == "__main__":
